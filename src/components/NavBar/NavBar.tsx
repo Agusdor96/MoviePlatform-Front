18bc3eb5b797161ebdb740bd746e8ac3ba6bc7bf
agusdor96 <miroetto12@gmail.com>
2024-11-16 15:15:18 -0300

    fix NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from './Navbar.module.css';
+import styles from './NavBar.module.css';
 
 interface NavbarProps {
   onSearch: (search: string) => void;
